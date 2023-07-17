77ce1c3b9d310b7e042c4eeb96bd88b02e4bc74a
Samantha Wittke <32324155+samumantha@users.noreply.github.com>
2023-07-17 13:03:08 +0300

    add book theme

--- conf.py
+++ conf.py
@@ -14,7 +14,7 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['myst_parser']
+extensions = ['myst_parser', 'sphinx-book-theme']
 
 source_suffix = ['.rst','.md']
     
