edba998134dedec39fb1879bf2a9eea337c10c80
Samantha Wittke <32324155+samumantha@users.noreply.github.com>
2023-07-21 11:48:18 +0300

    -_ mixup fix

--- conf.py
+++ conf.py
@@ -15,7 +15,7 @@ release = '0.1'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = ['myst_parser', 
-              'sphinx-togglebutton']
+              'sphinx_togglebutton']
 
 source_suffix = ['.rst','.md']
     
